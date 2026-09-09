83c0711bc84f52e3630d40f10428faed827031db
TWildP <aku.keto@gmail.com>
2026-09-09 09:57:08 +0300

    Bank Balace Added
    You now start with 5 euros to play with and each game costs 1 euro. If you win, you get 3 euros. You can also exit the game after playing.

--- Lucky7/src/Lucky7.java
+++ Lucky7/src/Lucky7.java
@@ -4,21 +4,47 @@ public class Lucky7 {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
 
+        int bank = 5;
+        String input = "";
         Random rng = new Random();
             int num1;
             int num2;
             int num3;
 
-        num1 = rng.nextInt(10) + 1;
-        num2 = rng.nextInt(10) + 1;
-        num3 = rng.nextInt(10) + 1;
+        System.out.println("Current Bank Balace: " + bank + " Euros");
+        System.out.println();
 
-        System.out.println(num1 + " " + num2 + " " + num3);
-            if (num1 == 7 || num2 == 7 || num3 == 7) {
-                System.out.println("Winner!");
-            } else {
-                System.out.println("You Lose");
-            }
+        while (bank > 0) {
+            System.out.println("GAMBLING TIME!!!");
+            System.out.println("1 Euro / Game");
+            System.out.println("Press Enter to Play!");
+                in.nextLine();
+                bank--;
 
+            num1 = rng.nextInt(10) + 1;
+            num2 = rng.nextInt(10) + 1;
+            num3 = rng.nextInt(10) + 1;
+                System.out.println(num1 + " " + num2 + " " + num3);
+
+                if (num1 == 7 || num2 == 7 || num3 == 7) {
+                    System.out.println("You Win! Payout: 3 Euros!");
+                    bank = bank + 3;
+                    System.out.println();
+                } else {
+                    System.out.println("You Lose");
+                    System.out.println();
+                }
+                System.out.println("Current Bank Balace: " + bank + " Euros");
+                    if (bank == 0) {
+                        System.out.println("You Lost All Your Money and Went Homeless :(");
+                        System.out.println("G A M E  O V E R");
+                        break;
+                    }
+;                System.out.println("Play Again? y/n");
+                    input = in.nextLine();
+                        if (input.equals("n")) {
+                            break;
+                        }
+        }
     }
 }
